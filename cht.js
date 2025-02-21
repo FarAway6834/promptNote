fec857b44c57804e1789afa155b418c2d8e3d55e
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-21 12:14:54 +0900

    add cht.js

---
+++ cht.js
@@ -0,0 +1,36 @@
+if (!window.alreadyLoadedScript) {
+    window.alreadyLoadedScript = true;
+} else {
+
+const GET = (doc, src) => doc.querySelector(`div.${src.replace(/ /g,'.')}`);
+const adddom = txt => document.head.insertAdjacentHTML("beforeend", txt);
+
+const getMD = (web) => {
+const md = GET(web, 'container-lg px-3 my-5 markdown-body');
+GET(md, 'footer border-top border-gray-light mt-5 pt-3 text-right text-gray').remove();
+return {
+    css: `<link rel="stylesheet" href="${web.getElementsByTagName("link")[0].href}" />`,
+    md: md.outerHTML
+};};
+
+const parseHTML = text => (new DOMParser()).parseFromString(text, 'text/html');
+const innerHTMLasWebMD = (url, target) => fetch(url).then(res=>res.text()).then(parseHTML).then(getMD).then((parsedat) => {
+    adddom(parsedat.css);
+    target.outerHTML= parsedat.md;
+}).catch(err => console.error(err));
+const replaceWebMD = ob => innerHTMLasWebMD(ob.getAttribute("src"), ob);
+
+document.addEventListener("DOMContentLoaded", () => {
+    class Usr extends HTMLElement {connectedCallback() {replaceWebMD(this);}}
+    class GPT extends HTMLElement {connectedCallback() {replaceWebMD(this);}}
+    customElements.define("u-sr", Usr);
+    customElements.define("g-pt", GPT);
+});
+
+adddom("<script defer src = "https://faraway6834.github.io/promptNote/chat.js"></script>");
+}
+
+document.currentScript.remove();
+
+//stable way : `<script defer src="https://faraway6834.github.io/promptNote/cht.js"></script>`
+//un stable way : `<script defer src="http://pmpt.kro.kr"></script>`
